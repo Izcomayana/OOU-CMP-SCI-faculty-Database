eddde5dc9cd40f6b8e53a0e84c590763e76c87e8
45szn <israelxxn@example.com>
2025-03-12 12:10:26 +0100

    leadership section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Leadership from "./components/Leadership";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Hero />
         <div className="container mx-auto">
           <About />
+          <Leadership />
         </div>
       </div>
 
