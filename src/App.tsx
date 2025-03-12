f034bafc9cd57bcf1503e307f2539f525562be18
45szn <israelxxn@example.com>
2025-03-12 09:26:42 +0100

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button>Click me</Button>
-      </div>
+      <Header />
     </>
   );
 }
