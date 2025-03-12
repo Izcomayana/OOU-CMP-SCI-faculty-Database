0c60c2adf6e79ee3ecd67f36628e5b4aa989d209
45szn <israelxxn@example.com>
2025-03-12 11:38:46 +0100

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <div className="hidden lg:block">
         <Header />
+        <Hero />
       </div>
 
       <div className="block lg:hidden">
